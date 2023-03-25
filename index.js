21f8e30f03b1c4bf2618ebd9f53aabfcc9485b7f
raymondseyi <seyidami13@gmail.com>
2023-03-25 13:55:42 +0100

    crud operations

--- index.js
+++ index.js
@@ -47,20 +47,58 @@ let studentModel = mongoose.model("students",studentSchema)
 app.get("/signup",(req,res)=>{
     res.render("signup")
 })
+app.get("/dashboard",(req,res)=>{
+    studentModel.find()
+    .then((response)=>{
+        res.render("dashboard",{response})
+        console.log(response)
+    })
+    .catch((err)=>{
+        res.render("dashboard")
+        console.log(err)
+    })
+})
 app.post("/register",(req,res)=>{
     console.log(req.body)
     let form = new studentModel(req.body)
     form.save().then(()=>{
         console.log("saved successfully")
-        studentModel.find().then((response)=>{
-            console.log(response)
-        })
+        res.redirect("/dashboard")
+        // res.render("dashboard")
     }).catch((err)=>{
         console.log("itaf refused to save")
         console.log(err)
+
     })
 
+})
+app.post("/delete",(req,res)=>{
+    console.log(req.body.id)
+    studentModel.findByIdAndDelete(req.body.id)
+    .then((response)=>{
+        console.log("it has been deleted")
+        res.redirect("/dashboard")
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+})
+app.post("/edit",(req,res)=>{
+    console.log(req.body)
+    let {firstname,lastname,email,password} = req.body
+    studentModel.findByIdAndUpdate(req.body.id,{firstname,lastname,email,password})
+    .then(()=>{
+        console.log("updated successfully")
+        res.redirect("/dashboard")
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
 })
 app.listen(5000,()=>{
     console.log("app has started")
 })
+// install thunder client extension on vscode
+// download and install insomnia and postman
+// create an account with npmjs.com
+// create an account with cloudinary.com
